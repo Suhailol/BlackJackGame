e2ebf75fb92111b19fa2086e64f6c98bb03f6bb4
Suhail <suhailmoidin96@gmail.com>
2023-01-14 23:21:28 +0530

    "upgrade"

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let playerEl = document.getElementById("player-el")
+let imgEl = document.getElementById("card-img")
 
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
+    changeImage(randomNumber)
     if (randomNumber > 10) {
         return 10
     } else if (randomNumber === 1) {
@@ -56,4 +58,52 @@ function newCard() {
         cards.push(card)
         renderGame()        
     }
+}
+
+function changeImage(cardNumber){
+    let imgUrl = "images/backofcard.png"
+    switch(cardNumber) {
+        case 1:
+            imgUrl = "https://deckofcardsapi.com/static/img/AH.png"
+            break;
+        case 2:
+            imgUrl = "https://deckofcardsapi.com/static/img/2C.png"
+            break;
+        case 3:
+            imgUrl = "https://deckofcardsapi.com/static/img/3S.png"
+            break;
+        case 4:
+            imgUrl = "https://deckofcardsapi.com/static/img/4D.png"
+            break;
+        case 5:
+            imgUrl = "https://deckofcardsapi.com/static/img/5H.png"
+            break;
+        case 6:
+            imgUrl = "https://deckofcardsapi.com/static/img/6C.png"
+            break;
+        case 7:
+            imgUrl = "https://deckofcardsapi.com/static/img/7S.png"
+            break;
+        case 8:
+            imgUrl = "https://deckofcardsapi.com/static/img/8D.png"
+            break;
+        case 9:
+            imgUrl = "https://deckofcardsapi.com/static/img/9H.png"
+            break;
+        case 10:
+            imgUrl = "https://deckofcardsapi.com/static/img/0D.png"
+            break;
+        case 11:
+            imgUrl = "https://deckofcardsapi.com/static/img/JC.png"
+            break;
+        case 12:
+            imgUrl = "https://deckofcardsapi.com/static/img/QS.png"
+            break;
+        case 13:
+            imgUrl = "https://deckofcardsapi.com/static/img/KH.png"
+            break;
+        default:
+            break;
+      }
+      imgEl.src = imgUrl;
 }
